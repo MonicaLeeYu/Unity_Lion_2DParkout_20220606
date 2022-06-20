ccb47e8fdfc782b395fd9fb64af0aea604d1652d
MonicaLeeYu <105775406+MonicaLeeYu@users.noreply.github.com>
2022-06-20 10:28:53 +0800

    Update練習靜態API

--- Unity_Lion_2DParkout_20220606/Assets/Scripts/APIStatic.cs
+++ Unity_Lion_2DParkout_20220606/Assets/Scripts/APIStatic.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 namespace MonicaLeeAPIStatic
 {
     public class APIStatic : MonoBehaviour
-    {
+    { 
+        private Vector3 a = new Vector3(1, 1, 1);
+        private Vector3 b = new Vector3(22, 22, 22);
+
         private void Start()
         {
             #region 課堂練習
@@ -24,9 +27,27 @@ namespace MonicaLeeAPIStatic
             int rangeInt = Random.Range(1, 3);
             print("RandomRangeInt = " + rangeInt);
             #endregion
+
+
             int allcamerascount = Camera.allCamerasCount;
-            print("allcamerascount = " + allcamerascount);
-            print("應用程式的平台" + Application.platform);
+            print("所有攝影機 = " + allcamerascount);
+            print("應用程式的平台 = " + Application.platform);
+
+            print("物理睡眠臨界值設定為 = " + Physics.sleepThreshold );
+            
+            Time.timeScale = 0.8f;
+            print("時間大小設定為 0.8 (慢動作) = " + Time.timeScale);
+            Mathf.Round(9.999f);
+            print("對9.999去小數點(四捨五入) = " + Mathf.Round(9.999f));
+            Mathf.Ceil(9.999f);
+            print("對9.999去小數點(無條件進位) = " + Mathf.Ceil(9.999f));
+            Mathf.Floor(9.999f);
+            print("對9.999去小數點(無條件捨去) = " + Mathf.Floor(9.999f));
+
+            float dist = Vector3.Distance(a, b);
+            print("<color=red>取得兩點的距離 = " + dist +"</color>");
+
+            Application.OpenURL("https://unity.com/"); //開啟連結網址
 
         }
         private void Update()
@@ -35,6 +56,9 @@ namespace MonicaLeeAPIStatic
             int rangeInt = Random.Range(1, 3);
             print("RandomRangeInt = " + rangeInt);
             #endregion
+            //print("是否輸入任意鍵 = " + Input.anyKeyDown);
+            //print("遊戲經過時間 = " + Time.timeSinceLevelLoad);
+            print("<color=lightblue>是否按下按鍵(指定為空白鍵) = " + Input.GetKeyDown(KeyCode.Space) +"</color>");
         }
     }
 }
